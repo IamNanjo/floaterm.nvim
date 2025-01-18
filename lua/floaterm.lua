0889c6c1e0270611980216e0cdfc5a1fecf33fdf
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-18 20:33:10 +0200

    feat: Added config options

--- lua/floaterm.lua
+++ lua/floaterm.lua
@@ -6,20 +6,39 @@ local state = {
     win = nil,
 }
 
-M.setup = function() end
+---@class config Plugin configuration
+---@field auto_insert boolean Automatically enter insert mode. Default
+---@field width_relative float Relative width of floating window. Default value is 0.8
+---@field height_relative float Relative height of floating window. Default value is 0.8
+---@field border "none"|"single"|"double"|"rounded"|"solid"|"shadow"|string[] Border type of floating window
+---@
+local config = {
+    auto_insert = true,
+    width_relative = 0.8,
+    height_relative = 0.8,
+    border = "rounded",
+}
+
+--- Set up plugin configuration
+---@param opts config
+M.setup = function(opts)
+    for k, v in pairs(opts) do
+        config[k] = v
+    end
+end
 
 --- Create window configuration
 ---@return vim.api.keyset.win_config
 local function create_window_configuration()
-    local width = math.floor(vim.o.columns * 0.8)
-    local height = math.floor(vim.o.lines * 0.8)
+    local width = math.floor(vim.o.columns * config.width_relative)
+    local height = math.floor(vim.o.lines * config.height_relative)
 
     return {
         relative = "editor",
         width = width,
         height = height,
         style = "minimal",
-        border = "rounded",
+        border = config.border,
         col = math.floor((vim.o.columns - width) / 2),
         row = math.ceil((vim.o.lines - height) / 2),
         zindex = 10,
@@ -31,8 +50,8 @@ local function buffer_is_terminal(buffer)
 end
 
 --- Creates a floating window and enters it automatically
----@param config vim.api.keyset.win_config?
-local function open_floating_window(config)
+---@param win_config vim.api.keyset.win_config?
+local function open_floating_window(win_config)
     if not state.buf or not vim.api.nvim_buf_is_valid(state.buf) or not vim.api.nvim_buf_is_loaded(state.buf) then
         state.buf = vim.api.nvim_create_buf(false, true)
 
@@ -41,7 +60,7 @@ local function open_floating_window(config)
         end
     end
 
-    state.win = vim.api.nvim_open_win(state.buf, true, config or create_window_configuration())
+    state.win = vim.api.nvim_open_win(state.buf, true, win_config or create_window_configuration())
     state.open = true
 
     if not buffer_is_terminal(state.buf) then
@@ -49,16 +68,17 @@ local function open_floating_window(config)
     end
 
     vim.keymap.set({ "t" }, "<C-[>", "<c-\\><c-n>", { noremap = true, silent = true, buffer = state.buf })
-    vim.keymap.set({ "t" }, "<C-[><C-[>", M.floaterminal, { noremap = true, silent = true, buffer = state.buf })
-    vim.keymap.set({ "n" }, "<C-[>", M.floaterminal, { noremap = true, silent = true, buffer = state.buf })
+    vim.keymap.set({ "n" }, "<C-[>", M.floaterm, { noremap = true, silent = true, buffer = state.buf })
 
-    vim.api.nvim_command("startinsert")
+    if config.auto_insert then
+        vim.api.nvim_command("startinsert")
+    end
 
     vim.bo[state.buf].buflisted = false
     vim.bo[state.buf].bufhidden = "hide"
 end
 
-M.floaterminal = function()
+M.floaterm = function()
     if state.buf and not vim.api.nvim_buf_is_loaded(state.buf) then
         state.buf = nil
         state.win = nil
@@ -76,7 +96,7 @@ M.floaterminal = function()
     end
 end
 
-vim.api.nvim_create_user_command("Floaterminal", M.floaterminal, {})
+vim.api.nvim_create_user_command("Floaterm", M.floaterm, {})
 
 vim.api.nvim_create_autocmd("VimResized", {
     callback = function()
